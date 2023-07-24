f6eff2187551266208881a5367ddd9505ff11eae
jexnjeux <zieun.na@gmail.com>
2023-07-24 22:52:01 +0900

    feat(LV.0): 문자열 반복해서 출력하기

---
+++ src/programmers/level1/P181950.java
@@ -0,0 +1,20 @@
+package programmers.level1;
+
+import java.util.Scanner;
+
+public class P181950 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String str = sc.next();
+    int n = sc.nextInt();
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < n; i++) {
+      sb.append(str);
+    }
+    String result = str.toString();
+    System.out.println(result);
+
+  }
+
+}
